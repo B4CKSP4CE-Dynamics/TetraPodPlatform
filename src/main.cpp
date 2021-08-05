ec49d98edb8c9df185187906a24ed2234532657a
aanper <mail@s3f.ru>
2021-08-05 21:16:50 +0300

    remove servos

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@ int max = 2480;
 
 int led = 13;
 
+/*
 struct LegServos
 {
     Servo s0, s1, s2;
 };
 
-
-
 LegServos l0, l1;
+*/
 void setup() {
-    // put your setup code here, to run once:
+    /*
     l1.s0.attach(9);
     l1.s1.attach(10);
     l1.s2.attach(11);
@@ -24,6 +24,7 @@ void setup() {
     l0.s0.attach(8);
     l0.s1.attach(7);
     l0.s2.attach(6);
+    */
 
     pinMode(led, OUTPUT);
 }
@@ -35,10 +36,9 @@ void loop() {
     // delay(d);
     // l0.s1.write(00);
     // delay(d);
-    //servo.write(30);
+
     digitalWrite(led, HIGH);
     delay(250);
     digitalWrite(led, LOW);
     delay(250);
-    // put your main code here, to run repeatedly:
 }
