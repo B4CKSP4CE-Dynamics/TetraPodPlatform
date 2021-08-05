72e39786378a2c98a303dce3a04e7622b0a6f33e
aanper <mail@s3f.ru>
2021-08-05 23:31:08 +0300

    reference values

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,13 @@ struct LegServos
 LegServos l0, l1, l2, l3;
 
 void setup() {
-    l1.s0.attach(9);
-    l1.s1.attach(10);
-    l1.s2.attach(11);
-
-    l1.s2.write(163); // straight = 163, 90deg = 111
-    l1.s1.write(133); // up = 35, straight = 133
-
     l0.s0.attach(8);
     l0.s1.attach(7);
     l0.s2.attach(6);
 
-    l0.s2.write(111); // straight = 17, 90deg = 111
+    l1.s0.attach(9);
+    l1.s1.attach(10);
+    l1.s2.attach(11);
 
     l2.s0.attach(3);
     l2.s1.attach(4);
@@ -33,7 +28,18 @@ void setup() {
 
     l3.s0.attach(2);
     l3.s1.attach(A5);
-    l3.s2.attach(A6);
+    l3.s2.attach(12);
+    
+    l0.s1.write(150); // up = 150, straight = 52
+    l0.s2.write(17); // straight = 17, 90deg = 111
+
+    l1.s2.write(163); // straight = 163, 90deg = 111
+    l1.s1.write(133); // up = 35, straight = 133
+
+    l2.s1.write(180); // straight = 23, 90deg = 120
+    l2.s2.write(23); // straight = 23, 90deg = 120
+
+    l3.s2.write(148); // straight = 148, 90deg = 46
 
     pinMode(led, OUTPUT);
 }
